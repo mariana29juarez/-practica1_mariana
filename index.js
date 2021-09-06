0ba966542607b052456871a2e1a69de4f5d34653
mariana29juarez <89815313+mariana29juarez@users.noreply.github.com>
2021-09-06 17:34:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+index.js
+function getIMC(){
+  var peso=parcefloat(document.getElementByID(´peso´).value);
+  var altura=parcefloat(document.getElementByID(´altura´).value);
+  var imc=peso/(altura*altura);
+  document.getElementByID("imc").innerHTML=imc;
